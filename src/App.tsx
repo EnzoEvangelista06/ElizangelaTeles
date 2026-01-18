8fb8699b666d1138bd8c79a20b9fd416ec66a3c9
Enzo Evangelista <enzoteuprogrammer@gmail.com>
2026-01-18 20:39:02 -0300

    :construction: const: initial structuring of the home section

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import "./styles/theme.css";
 import "./styles/global.css";
 import { Container } from "./components/Container";
 import { Header } from "./components/Header";
+import { Home } from "./Home";
 
 export function App() {
   return (
     <>
       <Container>
         <Header />
+      </Container>
+
+      <Container>
+        <Home />
       </Container>
     </>
   );
---
+++ src/Home/index.tsx
@@ -0,0 +1,26 @@
+import styles from "./home.module.css";
+
+export function Home() {
+  return (
+    <section className={styles.home}>
+      <div className={styles["text-section"]}>
+        <div className={styles.title}>
+          <h1>
+            Cuidar da sua saúde mental é um ato de <span>amor</span>
+          </h1>
+
+          <p>
+            Ofereço um espaço seguro e acolhedor para você se redescobrir,
+            superar desafios e construir uma vida mais equilibrada e
+            significativa.
+          </p>
+        </div>
+
+        <div className={styles["button-section"]}>
+          <a href="#contact">Agendar consulta online</a>
+          <a href="#specialties">Minhas especialidades</a>
+        </div>
+      </div>
+    </section>
+  );
+}
