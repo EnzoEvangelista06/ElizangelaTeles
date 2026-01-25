fe60f0ec1cc0a8ebefa11f18195155b8b69f60f2
Enzo Evangelista <enzoteuprogrammer@gmail.com>
2026-01-25 20:22:57 -0300

    :construction: const: partial completion of responsive header development

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,9 @@
-console.log("Olá, mundo!");
+const nav = document.querySelector("header nav");
+const buttons = document.querySelectorAll(".toggle i");
+
+for (const button of buttons) {
+  button.addEventListener("click", () => {
+    console.log("oi");
+    nav.classList.toggle("show");
+  });
+}
